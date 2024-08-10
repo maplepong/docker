f3ab09b4579e1a0e5d27350d318f3b6442f7e070
siwolee <siwolee@student.42seoul.kr>
2024-08-10 13:55:15 +0900

    fix: 대진표 보임 이제....

--- frontend/src/component/Tournament/TournamentSchedule.js
+++ frontend/src/component/Tournament/TournamentSchedule.js
@@ -1,19 +1,22 @@
 /* @jsx myReact.createElement */
 import myReact from "../../core/myReact.js";
-import "../../css/tournament/tournament_schedule.css"
+import "../../css/tournament/tournament_schedule.css";
 
-const TournamentSchedule = ({ braket, status, startGame }) => {
-  console.log(braket);
+const TournamentSchedule = ({ bracket, status, startGame }) => {
   return (
-  <div className="tournament-schedule-container">
-    <h1 className="schedule-title">Tournament Schedule</h1>
-    <p className="schedule-detail">브래킷: {bracket}</p>
-    <p className="schedule-detail">상태: {status}</p>
-    <div className="button-container">
-      <button className="start-button" onClick={startGame}>게임 시작</button>
-      <button className="start-button" onClick={startGame}>게임 시작</button>
+    <div className="tournament-schedule-container">
+      <h1 className="schedule-title">Tournament Schedule</h1>
+      <p className="schedule-detail">브래킷: {bracket.current}</p>
+      <p className="schedule-detail">상태: {status}</p>
+      <div className="button-container">
+        <button className="start-button" onClick={startGame}>
+          게임 시작
+        </button>
+        <button className="start-button" onClick={startGame}>
+          게임 시작
+        </button>
+      </div>
     </div>
-  </div>
   );
 };
 
